546ff36b755400730349a24f703cb701a4c99111
WMAITECH <wm@wmaitech.org>
2025-05-31 22:03:35 -0400

    Module cockpit RETROKO – interface visuelle CLI/WEB

---
+++ RETROKO_COCKPIT/display.py
@@ -0,0 +1,16 @@
+print("RETROKO VISION CLI/WEB COCKPIT")
+
+def show_header(title):
+    print("################################")
+    print(f#"\t RETROKO COCKPIT - " + title)
+    print("##################################"
+
+def menu_entrees():
+    print("- 1. Load Ampl/Capsule")
+    print("- 2. ViewS Image/plot")
+    print("- 3. Upscale / Export")
+    print(" - e. reset")
+
+def select_entry((opt="Enter option:
+")):
+    return input(opt)
