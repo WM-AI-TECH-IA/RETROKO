e590b72108b13f5d42cd703f47595ed7923f0531
WMAITECH <wm@wmaitech.org>
2025-06-03 21:46:53 -0400

    Correction de l'import invalide : remplacement 'import pedantic from json' par 'import json, from pydantic import BaseModel'

--- server/retroko_fastapi_server.py
+++ server/retroko_fastapi_server.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI, HTTPException, Depends
-import pedantic from json
+import json
+import from pydantic import BaseModel
 from auth import verify_token
 from terminal_controller import exec_command
 
 app = FastAPI()
 
-class TerminalRequest(pedantic.BaseModel):
+class TerminalRequest(BaseModel):
     command: string
 
 @app.post("/terminal/exec", dependencies=[Depends(verify_token)])
