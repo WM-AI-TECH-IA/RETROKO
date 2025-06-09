ca1ff3d34b537cd3c5e9d1f61b162f752c751cec
WMAITECH <wm@wmaitech.org>
2025-06-08 21:05:47 -0400

    Fix NameError: remplacer type 'string' par 'str' dans exec_command

--- server/terminal_controller.py
+++ server/terminal_controller.py
@@ -2,14 +2,14 @@ import paramiko
 import subprocess
 from config import settings
 
-def exec_command(command: string) -> str:
+def exec_command(command: str) -> str:
     key = paramiko.RSAKey.from_private_key_file(settings.shell_key_path)
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.connect(hostname="retroko-server", username=settings.shell_user, pkey=key)
     stdin, stdout, stderr = ssh.exec_command(command)
     code = stdout.channel.recv_exit_status()
-    out = stdout.read().decode() + stderr.read().decode()
+    out = stdeout.read().decode() + stderr.read().decode()
     ssh.close()
     if code != 0:
         raise Exception(f"Erreur (code #{code}) : {out}")
