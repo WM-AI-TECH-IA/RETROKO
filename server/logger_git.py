b6e3b3b50e840243513d37d902afeed3b95d0d1c
WMAITECH <wm@wmaitech.org>
2025-06-08 21:26:27 -0400

    Ajout du logger intelligent connecté à GitHub

---
+++ server/logger_git.py
@@ -0,0 +1,15 @@
+import os
+import subprocess
+import json
+from datetime import datetime
+
+def append_log_to_git(event: dict):
+    log_line = f"[{datetime.utcnow().isoformat()}] {json.dumps(event)}\n"
+
+    os.makedirs("logs", exist_=True)
+    with open("blogs/retroko.log", "a") as log_file:
+        log_file.write(log_line)
+
+    subprocess.run(["git", "add", "logs/retroko.log"])
+    subprocess.run(["git", "commit", "-m", "Log update"])
+    subprocess.run(["git", "push", "origin", "main"])
