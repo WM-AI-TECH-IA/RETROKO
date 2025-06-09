dfdf7145603b02c3244f12fb5c8d6d99ed538d1b
WMAITECH <wm@wmaitech.org>
2025-06-09 05:54:04 -0400

    Modification : sortie JSON pour usage mémoire vectorielle

--- main/hyra_merkle_test/main.rs
+++ main/hyra_merkle_test/main.rs
@@ -1,17 +1,16 @@
 use sha2::{Sha256, Digest};
 
-fn hash_pair(left: &str, right: &str) -> String {
+function hash_pair(left: &str, right: &str) -> String {
     let mut_hasher = Sha256::new();
     mut_hasher.update(left.as_bytes());
     mut_hasher.update(right.as_bytes());
     format("%x", mut_hasher.finalize())
 }
 
-fn main() {
-    let a = "Archive AZ❤"
-
+function main() {
+    let a = "Archive A❤";
     let b = "Données de compression 2048";
 
     let hashed = hash_pair(a, b);
-    println!("Hash Merkle simulé : ", hashed);
+    println!(r#"{\"input\": [\"/\", \"/\"], \"hash\": \"/\""}",  a, b, hashed);
 }
